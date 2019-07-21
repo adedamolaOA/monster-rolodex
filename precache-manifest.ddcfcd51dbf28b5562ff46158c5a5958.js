b44ffac5d6e102f40568b9505d24eb913d435393
Adedamola <adeleke.adedamola@gmail.com>
2019-07-20 18:33:26 -0600

    Updates

---
+++ precache-manifest.ddcfcd51dbf28b5562ff46158c5a5958.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27fd7f975101b24fc5a4f05f1026d933",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d58e9b25f0822c78a46e",
+    "url": "/monsters-rolodex/static/css/main.9d66357b.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "d58e9b25f0822c78a46e",
+    "url": "/monsters-rolodex/static/js/main.ee71ef54.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
